58b281344036a36f8232f5b518e88dbd2965e04f
Jarred Capellman <jcapellman@hotmail.com>
2016-07-05 15:31:42 -0700

    Updated the Shaders to use UTF-8 instead of ANSI

--- Game Assets/MBS1/Shaders/ps_color.hlsl
+++ Game Assets/MBS1/Shaders/ps_color.hlsl
@@ -1,4 +1,23 @@
-float4 main(float4 position : SV_POSITION) : SV_TARGET
+﻿struct PSInput
 {
-   return float4(1.0, 0.0, 0.0, 1.0);
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD;
+};
+
+Texture2D g_texture : register(t0);
+SamplerState g_sampler : register(s0);
+
+PSInput VSMain(float4 position : POSITION, float4 uv : TEXCOORD)
+{
+	PSInput result;
+
+	result.position = position;
+	result.uv = uv;
+
+	return result;
+}
+
+float4 PSMain(PSInput input) : SV_TARGET
+{
+	return g_texture.Sample(g_sampler, input.uv);
 }
--- Game Assets/MBS1/Shaders/vs_color.hlsl
+++ Game Assets/MBS1/Shaders/vs_color.hlsl
@@ -1,4 +1,23 @@
-float4 main(float4 position : POSITION) : SV_POSITION
+﻿struct PSInput
 {
-   return position;
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD;
+};
+
+Texture2D g_texture : register(t0);
+SamplerState g_sampler : register(s0);
+
+PSInput VSMain(float4 position : POSITION, float4 uv : TEXCOORD)
+{
+	PSInput result;
+
+	result.position = position;
+	result.uv = uv;
+
+	return result;
+}
+
+float4 PSMain(PSInput input) : SV_TARGET
+{
+	return g_texture.Sample(g_sampler, input.uv);
 }
