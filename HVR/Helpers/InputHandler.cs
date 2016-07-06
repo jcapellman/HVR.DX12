659d9ffeed85bb36076c8c67a1e42ab36798e606
Jarred Capellman <jcapellman@hotmail.com>
2016-07-05 19:24:03 -0700

    Added DirectInput Wrapper Class

---
+++ HVR/Helpers/InputHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+using SharpDX.DirectInput;
+
+namespace HVR.Helpers {
+    public class InputHandler {
+        private Keyboard _keyboard;
+
+        public InputHandler() {
+            var directInput = new DirectInput();
+
+            var devices = directInput.GetDevices();
+
+            var deviceKeyboard = devices.Where(a => a.Type == DeviceType.Keyboard).FirstOrDefault();
+
+            _keyboard = new Keyboard(directInput);
+
+            _keyboard.Properties.BufferSize = 128;
+            _keyboard.Acquire();
+        }
+
+        public KeyboardUpdate[] CheckInput() {
+            _keyboard.Poll();
+
+            return _keyboard.GetBufferedData();
+        }
+    }
+}
